ae63985a2b926920046197ee78bd3e69688ce417
Igor-prog41 <61401897+Igor-prog41@users.noreply.github.com>
2022-07-03 08:22:14 -0600

    ternary_operator

---
+++ ternary_operator.py
@@ -0,0 +1,12 @@
+# t = int(input("input the person's temperature :"))
+# health = "healthy" if t<37 else "ill"
+# print(f"person is {health}")
+# num_of_day = int(input("Enter the number of the day of the week :"))
+# day = "weekend" if num_of_day in [6, 7] else "workday"
+# print(day)
+# number = (int(input("enter number")))
+# num = number * 2 if number > 0 else abs(number)
+# print(num)
+n = int(input("enter number :"))
+is_lucky = "Lucky number" if n % 9 == 0 else "Not a lucky number"
+print(is_lucky)
